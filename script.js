993f385a8bd8282dc626d97b5baa2ac28bd1c35b
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-30 17:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function show(x,y)
+{
+    let count=0;
+    const t=setInterval(()=>{
+        const a=new Date();
+        const b=a.toLocaleString();
+        document.getElementById('timer').innerHTML = b;
+        count++;
+        if(count===y)
+            clearInterval(t)
+    },x*1000)
+}
+show(1,10)
